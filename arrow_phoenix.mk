4f7c1a6e16bcda8c7a78fda82f287d773fc3d770
Shripal Jain <shripal17@gmail.com>
2023-04-15 00:42:09 +0530

    phoenix: Arrow-ify
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- arrow_phoenix.mk
+++ arrow_phoenix.mk
@@ -10,24 +10,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Bliss stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+# Inherit some common ArrowOS stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bliss_phoenix
+PRODUCT_NAME := arrow_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Bliss and build type
-BLISS_BUILDTYPE=OFFICIAL
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
-TARGET_GAPPS_ARCH := arm64
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Arrow Specific Props
+ARROW_BUILD_TYPE := UNOFFICIAL
+
+DEVICE_MAINTAINER := shripal17
