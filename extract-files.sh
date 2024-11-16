3871d229e825f9ca5e4748e451ab14330ffe3730
Signed-off-by: lahaina <nattolecats@gmail.com>
2024-11-16 13:50:05 +0530

    phoenix: Link libwvhidl against libcrypto from vndk 33
    * It depends on symbol CBS_init which is removed from the latest libcrypto.
    
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.3-service.widevine": cannot locate symbol "CBS_init" referenced by "/vendor/lib64/libwvhidl.so"...
    
    Change-Id: I835f53be4970754bfffc37e8a5f5ddf0d16995ff
    
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,10 @@ function blob_fixup() {
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml )
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
     ;;
+    vendor/lib/libwvhidl.so)
+        [ "$2" = "" ] && return 0
+        "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
+        ;;
     vendor/lib64/libgoodixhwfingerprint.so )
     "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
     ;;
